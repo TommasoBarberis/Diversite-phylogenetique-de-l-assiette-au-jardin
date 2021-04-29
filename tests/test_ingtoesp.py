304518ab99177184862d69597326dc081fc11c6c
Tommaso <tommaso.barberis@etu.univ-lyon1.fr>
2021-04-29 17:34:28 +0200

    test pipeline 5

--- tests/test_ingtoesp.py
+++ tests/test_ingtoesp.py
@@ -1,7 +1,7 @@
 import lib.ing_to_esp as ite
 
-ingredients = {'liqueur': [['liqueur', 'liqueur'], '1.5', ['cl', 'cl']], 'sucre vanillé': [['sucre vanillé', 'sucre vanillé'], '1', ['paquet', 'paquets']], 'levure': [['levure', 'levure'], '0.5', ['paquet', 'paquets']], 'jus de citron': [['jus de citron', 'jus de citron'], '0.5', ['', '']], 'farine': [['farine', 'farine'], '150', ['g', 'g']], 'sucre': [['sucre', 'sucre'], '140', ['g', 'g']], 'oeuf': [['oeuf', 'oeufs'], '3', ['', '']]}
 
-def test_rechercheGlobale(ingredients):
+def test_rechercheGlobale():
+    ingredients = {'liqueur': [['liqueur', 'liqueur'], '1.5', ['cl', 'cl']], 'sucre vanillé': [['sucre vanillé', 'sucre vanillé'], '1', ['paquet', 'paquets']], 'levure': [['levure', 'levure'], '0.5', ['paquet', 'paquets']], 'jus de citron': [['jus de citron', 'jus de citron'], '0.5', ['', '']], 'farine': [['farine', 'farine'], '150', ['g', 'g']], 'sucre': [['sucre', 'sucre'], '140', ['g', 'g']], 'oeuf': [['oeuf', 'oeufs'], '3', ['', '']]}
     species = ite.recherche_globale(ingredients)
     assert len(species) == len(ingredients)
