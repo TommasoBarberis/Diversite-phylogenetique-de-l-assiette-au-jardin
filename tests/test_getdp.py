58548fb3a7fd749e95199f99c1a1e96de4b9e325
Tommaso <tommaso.barberis@etu.univ-lyon1.fr>
2021-04-30 10:38:12 +0200

    test_getdp.py update

--- tests/test_getdp.py
+++ tests/test_getdp.py
@@ -1,9 +1,10 @@
 import lib.get_dp as gdp
 
 
-def test_length_root_to_knot():
-    dict_lengths = gdp.length_root_to_knot("Tree_test.txt")
-    f = open("Tree_test.txt", "r", encoding="utf8")
+tree = Path(__file__).parent.joinpath("Tree_test.txt")
+def test_length_root_to_knot(tree):
+    dict_lengths = gdp.length_root_to_knot(tree)
+    f = open(tree, "r", encoding="utf8")
     tree = f.read()
     tree = tree.replace('[', '').replace(']', '').replace("'", "").replace(';', '')
     tree_length = tree.count(",") + tree.count("(")
