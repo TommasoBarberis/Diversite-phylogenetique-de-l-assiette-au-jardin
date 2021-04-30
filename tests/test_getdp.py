5c9cee2e6398e40c3bae65e7184f55b544838f0f
Tommaso <tommaso.barberis@etu.univ-lyon1.fr>
2021-04-30 11:27:39 +0200

    fix tests for ingproperties and getdp

--- tests/test_getdp.py
+++ tests/test_getdp.py
@@ -1,10 +1,11 @@
 import lib.get_dp as gdp
+import os
 
 
 def test_length_root_to_knot():
-    tree = "Tree_test.txt"
-    dict_lengths = gdp.length_root_to_knot(tree)
-    f = open(tree, "r", encoding="utf8")
+    file_path = (os.path.dirname(__file__))+"/Tree_test.txt"
+    dict_lengths = gdp.length_root_to_knot(file_path)
+    f = open(file_path, "r", encoding="utf8")
     tree = f.read()
     tree = tree.replace('[', '').replace(']', '').replace("'", "").replace(';', '')
     tree_length = tree.count(",") + tree.count("(")
--- tests/test_ingproperties.py
+++ tests/test_ingproperties.py
@@ -1,9 +1,13 @@
+import sys 
+import os
 import lib.ing_properties as ip
 import lib.get_ing as gi
+import xlrd
 
 
 def test_getNutInfo():
-    book = ip.openBook("../data/Table_Ciqual_2020_FR_2020_07_07.xls")
+    file_path = "data/Table_Ciqual_2020_FR_2020_07_07.xls"
+    book = xlrd.open_workbook(file_path)
     ing = "Poivre"
     assert len(ip.getNutInfo(ing, book)) == 5
 
@@ -12,4 +16,4 @@ def test_dryMatterDicUpdate():
     dict_nut = ip.getDictNut(ingredients)
     dry_matter = ip.dryMatterDicUpdate(ingredients, dict_nut)
     for ing in dry_matter:
-        assert (dry_matter[ing][0] < ingredients[ing][1])
+        assert (float(dry_matter[ing][0]) < float(ingredients[ing][1]))
