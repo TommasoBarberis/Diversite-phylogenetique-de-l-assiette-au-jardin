345b81ecbbce0370ead11c53ddc1c9565fc3c1c9
Tommaso <tommaso.barberis@etu.univ-lyon1.fr>
2021-05-06 11:09:45 +0200

    tests/test_gui.py update

--- tests/test_gui.py
+++ tests/test_gui.py
@@ -1,14 +1,15 @@
 import GUI as gui 
 
-ingredients = {'lardons': [['lardons', 'lardons'], '1', ['paquet', 'paquets']], 'pomme de terre': [['pomme de terre', 'pommes de terre'], '3', ['', '']], 'brick': [['brick', 'brick'], '8', ['feuille', 'feuilles']], 'raclette': [['raclette', 'raclette'], '1', ['paquet', 'paquets']], 'oignon': [['oignon', 'oignons'], '1', ['', '']]}
 
-def test_missing_species(ingredients):
+def test_missing_species():
+    ingredients = {'lardons': [['lardons', 'lardons'], '1', ['paquet', 'paquets']], 'pomme de terre': [['pomme de terre', 'pommes de terre'], '3', ['', '']], 'brick': [['brick', 'brick'], '8', ['feuille', 'feuilles']], 'raclette': [['raclette', 'raclette'], '1', ['paquet', 'paquets']], 'oignon': [['oignon', 'oignons'], '1', ['', '']]}
     species = {'lardons': 'Sus scrofa domesticus', 'pomme de terre': 'Solanum tuberosum', 'oignon': 'Allium cepa', 'raclette': 'Bos taurus'}
     resultats = gui.missing_species(ingredients, species)
     assert resultats[1] == False
 
 
-def test_missing_nutrition(ingredients):
+def test_missing_nutrition():
+    ingredients = {'lardons': [['lardons', 'lardons'], '1', ['paquet', 'paquets']], 'pomme de terre': [['pomme de terre', 'pommes de terre'], '3', ['', '']], 'brick': [['brick', 'brick'], '8', ['feuille', 'feuilles']], 'raclette': [['raclette', 'raclette'], '1', ['paquet', 'paquets']], 'oignon': [['oignon', 'oignons'], '1', ['', '']]}
     resultats = gui.missing_nutrition(ingredients)
     if resultats[0] == []:
         assert resultats[1] == True
